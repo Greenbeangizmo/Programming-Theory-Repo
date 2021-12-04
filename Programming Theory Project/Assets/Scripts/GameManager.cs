4587d45b8395f6895af28e6d1112746661fb4101
Greenbeangizmo <94251920+Greenbeangizmo@users.noreply.github.com>
2021-12-04 13:08:30 -0600

    title screen
    - added main and menu scenes
    - added title screen text
    - added start button

---
+++ Programming Theory Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
